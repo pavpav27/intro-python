c4dfdfc3f7ee6d2c6b514ff8a255447b7ce2f562
Pavlo Pavlovskyi <pavpav@kvn.no>
2026-09-02 10:31:22 +0200

    gjettespill

---
+++ intro_python.py
@@ -0,0 +1,21 @@
+# generer ett random tall
+
+# 10 forsøk
+
+import random
+
+rettTall = random.randint(0, 1000)
+
+for forsok in range(1, 11):
+    gjett = int(input("Gjett et tall mellom 0 og 1000: "))
+
+    if gjett == rettTall:
+        print("Riktig! Du fant tallet!")
+        break
+
+    forsok_igjen = 10 - forsok
+    print("Du har", forsok_igjen, "forsøk igjen.")
+
+else:
+    print("Du har brukt opp alle forsøkene.")
+    print("Du har tapt.")
